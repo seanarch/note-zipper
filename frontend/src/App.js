5f8e8e5843d67bc631396f859bac2ac5d95e16d3
seanarch <zhuqingyun@outlook.com>
2023-06-12 14:41:32 -0600

    Added style to landingpage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import LandingPage from "./screens/LandingPage/LandingPage";
 import "./App.css";
 
 function App() {
   return (
     <div>
       <Header />
-      <main style={{ minHeight: "93vh" }}></main>
+      <main>
+        <LandingPage />
+      </main>
       <Footer />
     </div>
   );
---
+++ frontend/src/screens/LandingPage/LandingPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container, Row } from "react-bootstrap";
+import "./LandingPage.css";
+
+const LandingPage = () => {
+  return (
+    <div className="main">
+      <Container>Landing</Container>
+    </div>
+  );
+};
+
+export default LandingPage;
