7273ae1b33ebe1194adbf17234fc01999caa626a
seanarch <zhuqingyun@outlook.com>
2023-06-12 15:48:13 -0600

    Added accordion on notes

--- frontend/src/screens/MyNotes/MyNotes.js
+++ frontend/src/screens/MyNotes/MyNotes.js
@@ -1,8 +1,72 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import MainScreen from "../../components/MainScreen";
+import { Accordion, Button, Card, Badge } from "react-bootstrap";
+import notes from "../../data/notes";
 
 const MyNotes = () => {
-  return <MainScreen title="Welcome back ...">My Notes</MainScreen>;
+  const deleteHandler = (id) => {
+    if (window.confirm("Are you sure?")) {
+    }
+  };
+  return (
+    <MainScreen title="Welcome back ...">
+      <Link to="createnote">
+        <Button style={{ marginLeft: 10, marginBottom: 6 }} size="lg">
+          Create New Note
+        </Button>
+      </Link>
+      {notes.map((note) => (
+        <Accordion>
+          <Card style={{ margin: 10 }}>
+            <Card.Header style={{ display: "flex" }}>
+              <span
+                style={{
+                  color: "black",
+                  textDecoration: "none",
+                  flex: 1,
+                  cursor: "pointer",
+                  alignSelf: "center",
+                  fontSize: 18,
+                }}
+              >
+                <Accordion.Toggle as={Card.Text} variant="link" eventKey="0">
+                  {note.title}
+                </Accordion.Toggle>
+              </span>
+              <div>
+                <Button href={`/note/${note._id}`}>Edit</Button>
+                <Button
+                  variant="danger"
+                  className="mx-2"
+                  onClick={() => deleteHandler(note._id)}
+                >
+                  Delete
+                </Button>
+              </div>
+            </Card.Header>
+            <Accordion.Collapse eventKey="0">
+              <Card.Body>
+                <h4>
+                  <Badge variant="success">Category - {note.category}</Badge>
+                </h4>
+                <blockquote className="blockquote mb-0">
+                  <p>
+                    {note.content}
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Integer posuere erat a ante.{" "}
+                  </p>
+                  <footer className="blockquote-footer">
+                    <cite title="Source Title">Created On - date</cite>
+                  </footer>
+                </blockquote>
+              </Card.Body>
+            </Accordion.Collapse>
+          </Card>
+        </Accordion>
+      ))}
+    </MainScreen>
+  );
 };
 
 export default MyNotes;
