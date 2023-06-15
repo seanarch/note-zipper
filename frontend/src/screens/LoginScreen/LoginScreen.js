24af9b9b53140479af92da9515dc930fead4e737
seanarch <zhuqingyun@outlook.com>
2023-06-14 21:34:14 -0600

    Added login screen style, update login screen

--- frontend/src/screens/LoginScreen/LoginScreen.js
+++ frontend/src/screens/LoginScreen/LoginScreen.js
@@ -1,6 +1,8 @@
 import React from "react";
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Row, Col } from "react-bootstrap";
 import MainScreen from "../../components/MainScreen";
+import { Link } from "react-router-dom";
+import "./LoginScreen.css";
 
 const LoginScreen = () => {
   return (
@@ -9,22 +11,27 @@ const LoginScreen = () => {
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
           <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
+          {/* <Form.Text className="text-muted">
             We'll never share your email with anyone else.
-          </Form.Text>
+          </Form.Text> */}
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
           <Form.Control type="password" placeholder="Password" />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        {/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
           <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
+        </Form.Group> */}
         <Button variant="primary" type="submit">
           Submit
         </Button>
       </Form>
+      <Row className="py-3">
+        <Col>
+          New Customer ? <Link to="/register">Register Here</Link>
+        </Col>
+      </Row>
     </MainScreen>
   );
 };
