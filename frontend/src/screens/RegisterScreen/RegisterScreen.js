6ca18a562632019368f2ada0524b57d333786b5d
seanarch <zhuqingyun@outlook.com>
2023-06-15 18:30:38 -0600

    basic functions of register page

--- frontend/src/screens/RegisterScreen/RegisterScreen.js
+++ frontend/src/screens/RegisterScreen/RegisterScreen.js
@@ -1,7 +1,125 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { Form, Row, Button, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import MainScreen from "../../components/MainScreen";
+import Loading from "../../components/Loading";
+import ErrorMessage from "../../components/ErrorMessage";
 
 const RegisterScreen = () => {
-  return <div>RegisterScreen</div>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmpassword, setConfirmpassword] = useState("");
+  const [pic, setPic] = useState(
+    "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg"
+  );
+  const [message, setMessage] = useState(null);
+  const [picMessage, setPicMessage] = useState(null);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    if (password !== confirmpassword) {
+      setMessage("Passwords do not match");
+    } else {
+      setMessage(null);
+      try {
+        const config = {
+          headers: {
+            "Content-type": "application/json",
+          },
+        };
+        setLoading(true);
+
+        const { data } = await axios.post(
+          "/api/users",
+          { name, pic, email, password },
+          config
+        );
+        setLoading(false);
+        localStorage.setItem("userInfo", JSON.stringify(data));
+      } catch (error) {
+        setError(error.response.data.message);
+      }
+    }
+    console.log(email);
+  };
+
+  return (
+    <MainScreen title="REGISTER">
+      <div className="loginContainer">
+        {error && <ErrorMessage variant="danger">{error}</ErrorMessage>}
+        {message && <ErrorMessage variant="danger">{message}</ErrorMessage>}
+        {loading && <Loading />}
+        <Form onSubmit={submitHandler}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              type="name"
+              value={name}
+              placeholder="Enter name"
+              onChange={(e) => setName(e.target.value)}
+            />
+            {/* <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text> */}
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              value={email}
+              placeholder="Enter email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            {/* <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text> */}
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              value={password}
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control
+              type="password"
+              value={confirmpassword}
+              placeholder="Confirm Password"
+              onChange={(e) => setConfirmpassword(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Profile Picture</Form.Label>
+            <Form.Control
+              type="password"
+              value={password}
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Register
+          </Button>
+        </Form>
+        <Row className="py-3">
+          <Col>
+            Have an Account ? <Link to="/login">Register Here</Link>
+          </Col>
+        </Row>
+      </div>
+    </MainScreen>
+  );
 };
 
 export default RegisterScreen;
