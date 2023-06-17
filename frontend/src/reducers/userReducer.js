66767d09ad3795111a2dda2108cd9d5a316f9d21
seanarch <zhuqingyun@outlook.com>
2023-06-17 10:55:23 -0600

    Add login reducer

--- frontend/src/reducers/userReducer.js
+++ frontend/src/reducers/userReducer.js
@@ -1,9 +1,21 @@
+import {
+  USER_LOGIN_FAIL,
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+  USER_LOGOUT,
+} from "../constants/userConstants";
+
 export const userLoginReducer = (state = {}, action) => {
   switch (action.type) {
-    case "":
-      break;
-
+    case USER_LOGIN_REQUEST:
+      return { loading: true };
+    case USER_LOGIN_SUCCESS:
+      return { loading: true, userInfo: action.payload };
+    case USER_LOGIN_FAIL:
+      return { loading: true, error: action.payload };
+    case USER_LOGOUT:
+      return {};
     default:
-      break;
+      return state;
   }
 };
