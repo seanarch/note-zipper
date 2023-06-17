03a22f54faea13227d7fb8b515883c7660e42e02
seanarch <zhuqingyun@outlook.com>
2023-06-17 17:28:48 -0600

    Update middleware

--- backend/controllers/noteControllers.js
+++ backend/controllers/noteControllers.js
@@ -1,4 +1,5 @@
 const Note = require("../models/noteModel");
+const asyncHandler = require("express-async-handler");
 
 const getNotes = asyncHandler(async (req, res) => {
   const notes = await Note.find();
--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const { getNotes } = require("../controllers/noteControllers");
+const { protect } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
-router.route("/").get();
+router.route("/").get(protect, getNotes);
 router.route("/create").post();
 router.route("/:id").get().put().delete();
 
