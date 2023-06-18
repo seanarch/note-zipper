b999eb6eb5b7443a920519f57d2a16c57d608bce
seanarch <zhuqingyun@outlook.com>
2023-06-18 14:47:47 -0600

    Add getnote by id

--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -1,11 +1,16 @@
 const express = require("express");
-const { getNotes, createNote } = require("../controllers/noteControllers");
+const {
+  getNotes,
+  createNote,
+  getNoteById,
+} = require("../controllers/noteControllers");
 const { protect } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
 router.route("/").get(protect, getNotes);
 router.route("/create").post(protect, createNote);
-router.route("/:id").get().put().delete();
+router.route("/:id").get(getNoteById);
+// .put().delete();
 
 module.exports = router;
