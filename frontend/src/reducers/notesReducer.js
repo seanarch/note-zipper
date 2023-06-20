ca55c7c8206888df877477bc821ed5936ce9148c
seanarch <zhuqingyun@outlook.com>
2023-06-20 16:02:45 -0600

    Add notecreatereducer

--- frontend/src/reducers/notesReducer.js
+++ frontend/src/reducers/notesReducer.js
@@ -2,6 +2,9 @@ import {
   NOTES_LIST_SUCCESS,
   NOTES_LIST_REQUEST,
   NOTES_LIST_FAIL,
+  NOTES_CREATE_SUCCESS,
+  NOTES_CREATE_REQUEST,
+  NOTES_CREATE_FAIL,
 } from "../constants/notesConstants";
 
 export const notesListReducer = (state = { notes: [] }, action) => {
@@ -11,6 +14,19 @@ export const notesListReducer = (state = { notes: [] }, action) => {
     case NOTES_LIST_SUCCESS:
       return { loading: false, notes: action.payload };
     case NOTES_LIST_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const notesCreateReducer = (state = { notes: [] }, action) => {
+  switch (action.type) {
+    case NOTES_CREATE_REQUEST:
+      return { loading: true };
+    case NOTES_CREATE_SUCCESS:
+      return { loading: false, notes: action.payload };
+    case NOTES_CREATE_FAIL:
       return { loading: false, error: action.payload };
     default:
       return state;
