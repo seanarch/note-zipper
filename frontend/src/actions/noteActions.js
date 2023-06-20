61f71a1b97990a28bd2a260f5ea3c9bb69df4605
seanarch <zhuqingyun@outlook.com>
2023-06-20 16:52:48 -0600

    Add update note action

--- frontend/src/actions/noteActions.js
+++ frontend/src/actions/noteActions.js
@@ -6,6 +6,9 @@ import {
   NOTES_CREATE_REQUEST,
   NOTES_CREATE_SUCCESS,
   NOTES_CREATE_FAIL,
+  NOTES_UPDATE_REQUEST,
+  NOTES_UPDATE_SUCCESS,
+  NOTES_UPDATE_FAIL,
 } from "../constants/notesConstants";
 
 export const listNotes = () => async (dispatch, getState) => {
@@ -77,6 +80,46 @@ export const createNoteAction =
           : error.message;
       dispatch({
         type: NOTES_CREATE_FAIL,
+        payload: message,
+      });
+    }
+  };
+
+export const updateNoteAction =
+  (id, title, content, category) => async (dispatch, getState) => {
+    try {
+      dispatch({
+        type: NOTES_UPDATE_REQUEST,
+      });
+
+      const {
+        userLogin: { userInfo },
+      } = getState();
+
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${userInfo.token}`,
+        },
+      };
+
+      const { data } = await axios.put(
+        `/api/notes/${id}`,
+        { title, content, category },
+        config
+      );
+
+      dispatch({
+        type: NOTES_UPDATE_SUCCESS,
+        payload: data,
+      });
+    } catch (error) {
+      const message =
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message;
+      dispatch({
+        type: NOTES_UPDATE_FAIL,
         payload: message,
       });
     }
